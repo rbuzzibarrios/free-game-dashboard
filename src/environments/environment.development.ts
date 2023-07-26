3aa62639ed2e1c65948cfe87e3ab1369fa092907
Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>
2023-07-26 12:20:59 -0400

    add setting to use vars from environments files (#10)
    update game.service.ts with configuration from environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: false,
+    proxyUrlCORSAnyWhere: 'https://cors-anywhere.herokuapp.com/',
+    apiFreeGameUrl: 'https://www.freetogame.com/api/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: true,
+    proxyUrlCORSAnyWhere: 'https://cors-anywhere.herokuapp.com/',
+    apiFreeGameUrl: 'https://www.freetogame.com/api/'
+};
