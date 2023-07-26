9a87f9072a8da32828dd96b0c854339798c9119a
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-07-26 01:00:10 -0400

    add getGameById method at game.service.ts
    use navigateToGameViewPage method at game-card.component.html
    
    implement navigateToGameViewPage at game-card.component.ts
    
    layout and show game details at game-view-page.component.html

--- src/app/game/pages/game-view-page/game-view-page.component.ts
+++ src/app/game/pages/game-view-page/game-view-page.component.ts
@@ -1,10 +1,25 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
+import {GameService} from "../../../core/services/game/game.service"
+import {Game} from "../../../core/models/game"
 
 @Component({
   selector: 'app-game-view-page',
   templateUrl: './game-view-page.component.html',
-  styleUrls: ['./game-view-page.component.sass']
+  styleUrls: ['./game-view-page.component.sass'],
+  providers: [GameService]
 })
-export class GameViewPageComponent {
+export class GameViewPageComponent implements OnInit{
   @Input() id: number | undefined = undefined
+
+  game: Game | undefined = undefined;
+
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    if (this.id) {
+      this.gameService
+          .getGameById(this.id as number)
+          .subscribe((game: Game) => this.game = game)
+    }
+  }
 }
